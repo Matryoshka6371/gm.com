e52f7e994e84cad8b0dbfb0c0d2a69ffac52cf02
Matryoshka <8738127+matryoshka1234@user.noreply.gitee.com>
2021-08-25 16:40:08 +0800

    a

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const cors=require("cors");
 const db=require("./libs/DBhelper");
 const file=require("./libs/fileHelper");
 const md5=require("md5");
+const { response } = require("express");
 
 
 // 创建对象
@@ -32,7 +33,7 @@ server.get("/api/index",async(request,response)=>{
 server.post("/account/reg",async function(request,response){
     let {user,pwd,phone}=request.body;
     // 2,准备sql语句
-    let sql ="INSERT INTO `userinfo` (`uName`,`uPwd`,`uPhone`) VALUES(?,?,?,?);"
+    let sql ="INSERT INTO `userinfo` (`uName`,`uPwd`,`uPhone`) VALUES(?,?,?);";
 
 
     // md5的二次加密
@@ -44,14 +45,50 @@ server.post("/account/reg",async function(request,response){
         //其他 (插入,修改,删除)返回的 受影响行数
         //不是undefined并且受影响行数大于等于1
         console.log(result);
-        let isReg=result&&result.affectRows;
+        let isReg=result&&result.affectedRows;
         response.json({
             msg:isReg?"注册成功":"注册失败",
             code:200
         })
     }catch(error){
         response.json({
-            msg:"注册失败",
+            msg:"该手机号已经被注册,请更换手机号进行注册",
+            err:error,
+            code:-200
+        })
+    }
+})
+
+// 201/08/25/16:30
+// 3.登录接口
+server.post("/accout/login",async (request,response)=>{
+    // 1.接受前端的数据(手机号和密码)
+    let {phone,pwd}=request.body;
+    // 2.准备sql语句
+    let sql="SELECT `uId`,`uName`,`uSex`,`uPhone` FROM `userinfo` WHERE uname=? AND upwd=? AND ustatus=1;";
+    let params=[
+        phone,
+        md5(md5(pwd)+"lxw")
+    ]
+    try{
+        let result=await db.exec(sql,params);
+        if(result&&result.length>=1){
+            // 查询语句返回数组
+            response.json({
+                msg:"登录成功!",
+                data:result,
+                code:200
+
+            })
+        }else{
+            response.json({
+                msg:"登录失败!",
+                code:200
+            })
+        }
+    }catch(error){
+        response.json({
+            msg:"登录失败!",
             err:error,
             code:-200
         })
